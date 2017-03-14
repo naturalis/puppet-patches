ec6da6fe99013f7d68d798517d474b145d043b6a
Atze de Vries <atze.devries@naturalis.nl>
2017-03-14 15:47:03 +0100

    fix

--- manifests/os_level/fix_puppet_repo.pp
+++ manifests/os_level/fix_puppet_repo.pp
@@ -5,7 +5,7 @@ class patches::os_level::fix_puppet_repo(
 ){
 
   exec {"fix puppet apt-repo" :
-    command => "/bin/bin/gpg --keyserver pgp.mit.edu --recv-key 7F438280EF8D349F && gpg -a --export EF8D349F | apt-key add - && sudo dpkg --configure -a && apt-get update",
+    command => "/usr/bin/gpg --keyserver pgp.mit.edu --recv-key 7F438280EF8D349F && gpg -a --export EF8D349F | apt-key add - && sudo dpkg --configure -a && apt-get update",
     unless  => "/usr/bin/apt-get check"
   }
 }
