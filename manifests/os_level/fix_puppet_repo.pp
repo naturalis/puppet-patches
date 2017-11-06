5440ad9eba04be005136512f36b10162d92c84ae
Hugo van Duijn <hugo.vanduijn@naturalis.nl>
2017-11-06 15:18:31 +0000

    changed keyserver

--- manifests/os_level/fix_puppet_repo.pp
+++ manifests/os_level/fix_puppet_repo.pp
@@ -6,7 +6,7 @@ class patches::os_level::fix_puppet_repo(
 ){
 
   exec {"fix puppet apt-repo" :
-    command => "/usr/bin/gpg --keyserver pgp.mit.edu --recv-key 7F438280EF8D349F && gpg -a --export EF8D349F | apt-key add - && sudo dpkg --configure -a && apt-get update",
+    command => "/usr/bin/gpg --keyserver keyserver.ubuntu.com --recv-key 7F438280EF8D349F && gpg -a --export EF8D349F | apt-key add - && sudo dpkg --configure -a && apt-get update",
     unless  => ["/usr/bin/apt-key list | grep -c EF8D349F"]
   }
 }
