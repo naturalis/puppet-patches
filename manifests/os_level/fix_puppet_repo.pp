59603cb4ce7d28805f21bff0f4cafbf7cb4f41a8
Hugo van Duijn <hugo.vanduijn@naturalis.nl>
2017-11-06 12:48:36 +0000

    updated puppetlabs repo patch

--- manifests/os_level/fix_puppet_repo.pp
+++ manifests/os_level/fix_puppet_repo.pp
@@ -1,11 +1,12 @@
 #
 #
+#    unless  => ["/usr/bin/apt-get check"] changed so install will always run when key is not available
 #
 class patches::os_level::fix_puppet_repo(
 ){
 
   exec {"fix puppet apt-repo" :
     command => "/usr/bin/gpg --keyserver pgp.mit.edu --recv-key 7F438280EF8D349F && gpg -a --export EF8D349F | apt-key add - && sudo dpkg --configure -a && apt-get update",
-    unless  => "/usr/bin/apt-get check"
+    unless  => ["/usr/bin/apt-key list | grep -c EF8D349F"]
   }
 }
